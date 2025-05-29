d28ea57840f778ae81065ec458eb8c43632089de
MarVinDev02 <m.v.paixaoo@hotmail.com>
2025-05-29 02:02:15 -0300

    dotnet

--- curso/Program/Pratica/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿        var cadastro = new Cadastro.CadastroGeral();
-        cadastro.adicionarPessoas(new Humano.Pessoa("Marcus", 30));
-        cadastro.mostrarListaDePessoas();
--- curso/record/record/record.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace record
-{
-    public class record
-    {
-        public int Id { get; set; }
-
-        public string Descricao { get; set; }
-    }
-}
